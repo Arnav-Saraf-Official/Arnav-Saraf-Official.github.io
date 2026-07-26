05bd4cd78cec733bfd5c242c9ec04420b3c6f347
arnav-saraf-official <reacharnavsaraf@gmail.com>
2026-07-26 16:33:34 -0500

    wrangler now handles gh

---
+++ wrangler/src/index.ts
@@ -0,0 +1,34 @@
+export default {
+    //@ts-expect-error-any-types
+    async fetch(request, env) {
+    const allowedOrigins = [
+      "https://arnav-saraf-official.github.io",
+      "http://localhost:5173",
+      "https://arnavsaraf.pages.dev"
+    ];
+
+    const origin = request.headers.get("Origin");
+    const corsHeaders = {
+      "Access-Control-Allow-Origin": allowedOrigins.includes(origin) ? origin : allowedOrigins[0],
+      "Access-Control-Allow-Methods": "GET, HEAD, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      "Content-Type": "application/json"
+    };
+
+    if (request.method === "OPTIONS") return new Response(null, { headers: corsHeaders });
+
+    const url = new URL(request.url);
+    
+    const githubUrl = `https://github.com${url.pathname}${url.search}`;
+
+    const githubResponse = await fetch(githubUrl, {
+      headers: {
+        "User-Agent": "Cloudflare-Worker-Proxy",
+        "Authorization": `Bearer ${env.GITHUB_TOKEN}`
+      }
+    });
+
+    const data = await githubResponse.text();
+    return new Response(data, { status: githubResponse.status, headers: corsHeaders });
+  }
+};
